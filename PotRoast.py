415634dd953bdc43b86b2f8dbcb6b950c2156386
NoodleShogun <38671132+NoodleShogun@users.noreply.github.com>
2018-04-23 15:39:00 -0700

    Roasts Updated
    Adds so roasts to the Roast list.

--- PotRoast.py
+++ PotRoast.py
@@ -8,6 +8,9 @@ import discord
 TOKEN = 'NDM4MDgyMTAyOTUzNzcxMDEw.Db_buQ._9gFilYm4oRyurUMPACaJ4DrLxs'
 client = discord.Client()
 
+roast1 = ["If I wanted to Commit Suicide I'd climb your Ego and Jump to your IQ", "Ur Mom Gay", 
+          "One might say that you lack an ability to process reasonable explanations for sticky situations"
+         ]
 
 
 
